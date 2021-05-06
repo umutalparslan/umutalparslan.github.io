640e31d676e90f9110c7fe99f77211c84a734e44
Umut Can Alparslan <umut_alparslan@hotmail.com>
2021-05-07 00:03:25 +0300

    added sdk versions

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,5 +26,44 @@ $(document).ready(function() {
         e.preventDefault();
         $(this).ekkoLightbox();
     });
-
+    var iosVL = document.querySelector(".ios-vl");
+    var iosEM = document.querySelector(".ios-em");
+    var andVL = document.querySelector(".and-vl");
+    var andEM = document.querySelector(".and-em");
+    function getIOSVL(){
+            $.ajax({
+                url: 'https://api.github.com/repos/relateddigital/visilabs-ios/releases/latest',
+                success: function(data){
+                    iosVL.innerHTML = "<strong>Current Version:</strong> " + data.tag_name;
+                }
+        });
+    }
+    function getANDVL(){
+        $.ajax({
+            url: 'https://api.github.com/repos/relateddigital/visilabs-android/releases/latest',
+            success: function(data){
+                andVL.innerHTML = "<strong>Current Version:</strong> " + data.tag_name;
+            }
+    });
+}
+function getIOSEM(){
+    $.ajax({
+        url: 'https://api.github.com/repos/relateddigital/euromessage-ios/releases/latest',
+        success: function(data){
+            iosEM.innerHTML = "<strong>Current Version:</strong> " + data.tag_name;
+        }
+});
+}
+function getANDEM(){
+$.ajax({
+    url: 'https://api.github.com/repos/relateddigital/euromessage-android/releases/latest',
+    success: function(data){
+        andEM.innerHTML = "<strong>Current Version:</strong> " + data.tag_name;
+    }
+});
+}
+    getIOSVL();
+    getANDVL();
+    getIOSEM();
+    getANDEM();
 });
